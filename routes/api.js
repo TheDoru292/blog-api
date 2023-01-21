5d5e52aeb8959a61ea7fcb2f93af4fa696a77ff1
thedoru <thedoruv2@gmail.com>
2023-01-21 22:52:31 +0100

    Api router

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,24 @@
-require("dotenv").config();
 var express = require("express");
 var router = express.Router();
 
+const comments = require("./comments");
+const auth = require("./auth");
+const post = require("./posts");
+
 const mongoose = require("mongoose");
-mongoose.connect = process.env.MONGODB_LINK;
+mongoose.connect(process.env.MONGODB_LINK);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+router.get("/", (req, res, next) => {
+  res.json({ success: true, message: "Hello!" });
 });
+
+router.use("/comment", comments);
+
+router.use("/auth", auth);
+
+router.use("/post", post);
 
 module.exports = router;
