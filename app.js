b7448f565d9a98186844752af0e54f11136fc21a
thedoru <thedoruv2@gmail.com>
2023-02-04 12:58:01 +0100

    cors settings

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 require("dotenv").config();
+const cors = require("cors");
 
 var apiRouter = require("./routes/api");
 
 var app = express();
 
+const corsOptions = {
+  origin: "http://localhost:3001",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -36,6 +44,8 @@ app.use(function (err, req, res, next) {
   } else if (err.status == 500) {
     errorMessage = "Internal server error.";
   }
+
+  console.log(err);
 
   // render the error page
   res.status(errorStatus);
